8e7e79026ac53bed874d758e7088b7e9ba853516
Mengxiao Lin <linmx0130@gmail.com>
2017-10-06 00:27:42 +0800

    reconstructed into RPN

---
+++ rpn.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+# Copyright 2017, Mengxiao Lin <linmx0130@gmail.com>
+
+"""
+RPN: Region Proposal Network
+"""
+
+import mxnet as mx
+
+def setConvWeights(lv: mx.gluon.nn.Conv2D, rv: mx.gluon.nn.Conv2D):
+    lv.weight.set_data(rv.weight.data())
+    lv.bias.set_data(rv.bias.data())
+
+class RPNFeatureExtractor(mx.gluon.Block):
+    def __init__(self, **kwargs):
+        super(RPNFeatureExtractor, self).__init__(**kwargs)
+        self.conv1_1 = mx.gluon.nn.Conv2D(channels=64, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=3, activation='relu')
+        self.conv1_2 = mx.gluon.nn.Conv2D(channels=64, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=64, activation='relu')
+        self.pool1 = mx.gluon.nn.MaxPool2D(pool_size=(2,2), strides=(2,2), padding=0, ceil_mode=False)
+
+        self.conv2_1 = mx.gluon.nn.Conv2D(channels=128, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=64, activation='relu')
+        self.conv2_2 = mx.gluon.nn.Conv2D(channels=128, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=128, activation='relu')
+        self.pool2 = mx.gluon.nn.MaxPool2D(pool_size=(2,2), strides=(2,2), padding=0, ceil_mode=False)
+
+        self.conv3_1 = mx.gluon.nn.Conv2D(channels=256, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=128, activation='relu')
+        self.conv3_2 = mx.gluon.nn.Conv2D(channels=256, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=256, activation='relu')
+        self.conv3_3 = mx.gluon.nn.Conv2D(channels=256, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=256, activation='relu')
+        self.pool3 = mx.gluon.nn.MaxPool2D(pool_size=(2,2), strides=(2,2), padding=0, ceil_mode=False)
+
+        self.conv4_1 = mx.gluon.nn.Conv2D(channels=512, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=256, activation='relu')
+        self.conv4_2 = mx.gluon.nn.Conv2D(channels=512, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=512, activation='relu')
+        self.conv4_3 = mx.gluon.nn.Conv2D(channels=512, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=512, activation='relu')
+        self.pool4 = mx.gluon.nn.MaxPool2D(pool_size=(2,2), strides=(2,2), padding=0, ceil_mode=False)
+        
+        self.conv5_1 = mx.gluon.nn.Conv2D(channels=512, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=512, activation='relu')
+        self.conv5_2 = mx.gluon.nn.Conv2D(channels=512, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=512, activation='relu')
+        self.conv5_3 = mx.gluon.nn.Conv2D(channels=512, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1), in_channels=512, activation='relu')
+
+    def init_by_vgg(self, ctx):
+        self.collect_params().initialize(mx.init.Normal(), ctx=ctx)
+        vgg16 = mx.gluon.model_zoo.vision.vgg16(pretrained=True)
+        setConvWeights(self.conv1_1, vgg16.features[0])
+        setConvWeights(self.conv1_2, vgg16.features[2])
+        setConvWeights(self.conv2_1, vgg16.features[5])
+        setConvWeights(self.conv2_2, vgg16.features[7])
+        setConvWeights(self.conv3_1, vgg16.features[10])
+        setConvWeights(self.conv3_2, vgg16.features[12])
+        setConvWeights(self.conv3_3, vgg16.features[14])
+        setConvWeights(self.conv4_1, vgg16.features[17])
+        setConvWeights(self.conv4_2, vgg16.features[19])
+        setConvWeights(self.conv4_3, vgg16.features[21])
+        setConvWeights(self.conv5_1, vgg16.features[24])
+        setConvWeights(self.conv5_2, vgg16.features[26])
+        setConvWeights(self.conv5_3, vgg16.features[28])
+
+    def forward(self, x, *args):
+        features = []
+        f = self.conv1_1(x)
+        f = self.conv1_2(f)
+        f = self.pool1(f)
+        
+        f = self.conv2_1(f)
+        f = self.conv2_2(f)
+        f = self.pool2(f)
+
+        f = self.conv3_1(f)
+        f = self.conv3_2(f)
+        f = self.conv3_3(f)
+        f = self.pool3(f)
+
+        f = self.conv4_1(f)
+        f = self.conv4_2(f)
+        f = self.conv4_3(f)
+        f = self.pool4(f)
+        f = self.conv5_1(f)
+        f = self.conv5_2(f)
+        f = self.conv5_3(f)
+        return f
+
+
+class DetectorHead(mx.gluon.Block):
+    def __init__(self,num_anchors, **kwargs):
+        super(DetectorHead, self).__init__(**kwargs)
+        self.conv1 = mx.gluon.nn.Conv2D(channels=512, kernel_size=(3, 3), padding=(1,1), activation='relu')
+        self.conv_cls = mx.gluon.nn.Conv2D(channels=2*num_anchors, kernel_size=(1, 1),padding=(0, 0))
+        self.conv_reg = mx.gluon.nn.Conv2D(channels=4*num_anchors, kernel_size=(1, 1), padding=(0, 0))
+    
+    def forward(self, feature, *args):
+        f = self.conv1(feature)
+        f_cls = self.conv_cls(f)
+        f_reg = self.conv_reg(f)
+        return f_cls, f_reg
+
+    def init_params(self, ctx):
+        self.collect_params().initialize(mx.init.Normal(), ctx=ctx)
+
--- train.py
+++ train.py
@@ -3,14 +3,14 @@
 
 from config import cfg
 from VOCDataset import VOCDataset
-from ssd import SSDFeatures, DetectorHead
+from rpn import RPNFeatureExtractor, DetectorHead
 import mxnet as mx
 from utils import random_flip, imagenetNormalize, img_resize, random_square_crop, bbox_overlaps
-from anchor_generator import ssd_generate_anchors, map_anchors
+from anchor_generator import generate_anchors, map_anchors
 
 def train_transformation(data, label):
     data, label = random_flip(data, label)
-    data, label = random_square_crop(data, label)
+    #data, label = random_square_crop(data, label)
     data = imagenetNormalize(data)
     return data, label
 
@@ -21,12 +21,10 @@ train_dataset = VOCDataset(annotation_dir=cfg.annotation_dir,
                            resize_func=img_resize)
 train_datait = mx.gluon.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
 ctx = mx.gpu(0)
-ssd_features = SSDFeatures()
-ssd_features.init_by_vgg(ctx)
-ssd_heads = [DetectorHead(21, 5), DetectorHead(21, 5), DetectorHead(21, 5), DetectorHead(21, 5)]
-
-for h in ssd_heads:
-    h.init_params(ctx)
+feature_extractor = RPNFeatureExtractor()
+feature_extractor.init_by_vgg(ctx)
+rpn_head = DetectorHead(9)
+rpn_head.init_params(ctx)
 
 for it, (data, label) in enumerate(train_datait):
     data = data.as_in_context(ctx)
@@ -36,31 +34,31 @@ for it, (data, label) in enumerate(train_datait):
     label = mx.nd.concatenate([background_bndbox, label], axis=0).reshape((1, -1, 5))
     label_count = label.shape[1]
     with mx.autograd.record():
-        features = ssd_features(data)
-        for fi, (f, dh) in enumerate(zip(features, ssd_heads)):
-            cls_head, reg_head = dh(f)
-            _fn, _fc, feature_height, feature_width = reg_head.shape
-            anchor_counts = _fc // 4
-            # only batch size=1 is supported
-            ref_anchors = ssd_generate_anchors(cfg.anchor_scales[fi] * h, ratios=cfg.anchor_ratios)
-            anchors = map_anchors(ref_anchors, reg_head.shape, h, ctx)
-            anchors = anchors.reshape((1, -1, 4, feature_height, feature_width))
-            anchors = mx.nd.transpose(anchors, (0, 3, 4, 1, 2))
-            anchors = anchors.reshape((-1, 4))
-            # So until now, anchors are N * 4, the order is [(H, W, A), 4]
-            overlaps = bbox_overlaps(anchors, label.reshape((-1, 4)))
-            overlaps = overlaps.reshape((1, feature_height, feature_width, anchor_counts, -1))
-            # Reshape the overlaps to [1, H, W, A, #{label}]
-            overlaps = mx.nd.transpose(overlaps, (0, 3, 1, 2, 4))
-            # Transpose overlaps to [1, A, H, W, #{label}]
-            bbox_assignment = mx.nd.argmax(overlaps, axis=4)
-            # Get bbox_assignment to [1, A, H, W]
-            label_extend = label[:,:,4].reshape(
+        f = feature_extractor(data)
+        rpn_cls, rpn_reg = rpn_head(f)
+        _fn, _fc, feature_height, feature_width = rpn_reg.shape
+        anchor_counts = _fc // 4
+        # only batch size=1 is supported
+        ref_anchors = generate_anchors(base_size=16)
+        anchors = map_anchors(ref_anchors, rpn_reg.shape, h, ctx)
+        anchors = anchors.reshape((1, -1, 4, feature_height, feature_width))
+        anchors = mx.nd.transpose(anchors, (0, 3, 4, 1, 2))
+        anchors = anchors.reshape((-1, 4))
+        # So until now, anchors are N * 4, the order is [(H, W, A), 4]
+        overlaps = bbox_overlaps(anchors, label.reshape((-1, 4)))
+        overlaps = overlaps.reshape((1, feature_height, feature_width, anchor_counts, -1))
+        # Reshape the overlaps to [1, H, W, A, #{label}]
+        overlaps = mx.nd.transpose(overlaps, (0, 3, 1, 2, 4))
+        # Transpose overlaps to [1, A, H, W, #{label}]
+        bbox_assignment = mx.nd.argmax(overlaps, axis=4)
+        # Get bbox_assignment to [1, A, H, W]
+        label_extend = label[:,:,4].reshape(
                     (1, 1, 1, 1, label_count)).broadcast_to(
                     (1, anchor_counts, feature_height, feature_width, label_count))
-            bbox_cls_gt = mx.nd.pick(label_extend, bbox_assignment)
-            from IPython import embed; embed()
-            break
+        bbox_cls_gt = bbox_assignment > 0 # RPN only tell whether there is an object
+        #TODO: bbox_reg_gt 
+        from IPython import embed; embed()
+        break
     #TODO
     if it >= 5:
         break
